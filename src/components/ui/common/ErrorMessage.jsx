4642077d8524f5da0edc280a4c3086b0cbf2e0c7
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-11-28 20:22:06 +0100

    cleanup removes comments

--- src/components/ui/common/ErrorMessage.jsx
+++ src/components/ui/common/ErrorMessage.jsx
@@ -1,8 +1,8 @@
 import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Import the required styles
+import "react-toastify/dist/ReactToastify.css";
 
 export const notify = (message, type = "error") => {
-  console.log(`Toast triggered with message: ${message}`); // Add this log
+  console.log(`Toast triggered with message: ${message}`);
   toast(message, {
     type,
     position: "top-center",
